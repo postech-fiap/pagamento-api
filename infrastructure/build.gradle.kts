9b2baafeb1c7bc54a96b12323ed44412601eec65
Mel <melmelissa1936@gmail.com>
2024-01-25 21:59:55 -0300

    refacts: add http client lib

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     implementation(project(":domain"))
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.2.1")
 
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:4.12.0")
