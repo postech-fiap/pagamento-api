ca89398c684f3e9c04ce242956a17bd43e692347
Mel <melmelissa1936@gmail.com>
2024-03-05 01:05:07 -0300

    refacts

--- domain/src/main/kotlin/br/com/fiap/pagamento/constants/Constants.kt
+++ domain/src/main/kotlin/br/com/fiap/pagamento/constants/Constants.kt
@@ -1,4 +1,4 @@
 package br.com.fiap.pagamento.constants
 
-const val PAGAMENTO_FINALIZADO_FILA = "pagamento-realizado"
+const val PAGAMENTO_FINALIZADO_FILA = "pagamento-finalizado"
 const val PEDIDO_CRIADO_FILA = "pedido-criado"
